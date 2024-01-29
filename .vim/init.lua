8261fa5bfc41da07f9767607fd826115411a07ad
Mathew Attlee <hello@codeinabox.com>
2024-01-29 11:00:42 +0000

    Switch to using init.lua

---
+++ .vim/init.lua
@@ -0,0 +1,11 @@
+-- Neovim configuration to extend .vimrc
+vim.cmd("source ~/.vimrc")
+
+-- Shortcut to reload ~/.vim/init.lua
+vim.keymap.set('n', '<Leader>r', '<cmd>luafile ~/.vim/init.lua<CR>')
+
+-- Turn off spell check in terminal
+vim.api.nvim_create_autocmd({"TermOpen"}, {
+  pattern = {"*"},
+  command = "setlocal nospell",
+})
--- .vim/init.vim
+++
@@ -1,8 +0,0 @@
-" Neovim configuration to extend .vimrc
-source ~/.vimrc
-
-" Shortcut to reload ~/.vim/init.vim
-nnoremap <Leader>r :source ~/.vim/init.vim<CR>
-
-" Turn off spell check in terminal
-autocmd TermOpen * setlocal nospell
