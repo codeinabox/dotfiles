28e475af4da26327f29e86ccf5c27f4c97e968b0
Mathew Attlee <hello@codeinabox.com>
2024-01-30 10:58:51 +0000

    Toggleterm

--- .vim/init.lua
+++ .vim/init.lua
@@ -9,3 +9,27 @@ vim.api.nvim_create_autocmd({"TermOpen"}, {
   pattern = {"*"},
   command = "setlocal nospell",
 })
+
+-- Toggleterm
+require("toggleterm").setup{}
+vim.g['test#strategy'] = "toggleterm"
+
+-- mason-lspconfig.nviM
+require("mason").setup()
+require("mason-lspconfig").setup {
+	ensure_installed = {
+		"prosemd_lsp",
+		"lua_ls",
+		"typos_lsp",
+		"vtsls"
+	},
+}
+
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function (server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {}
+	end,
+}
